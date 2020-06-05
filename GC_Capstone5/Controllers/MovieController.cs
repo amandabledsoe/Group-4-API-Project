fa1a1e800bb19f1dd8cb4fc1d145dea5c13796fc
andres-torres-2020 <andres.torres.192020@gmail.com>
2020-06-05 14:08:21 -0400

    Initial version.

---
+++ GC_Capstone5/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GC_Capstone5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GC_Capstone5.Controllers
+{
+    public class MovieController : Controller
+    {
+        //private readonly string _apiKey;
+        private readonly MovieDAL _movieDAL;
+
+        public MovieController(IConfiguration configuration)
+        {
+            _movieDAL = new MovieDAL(configuration);
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            Movie movie = await  _movieDAL.GetMovie(76341);
+            return View("Index", movie);
+        }
+    }
+}
---
+++ GC_Capstone5/Models/MovieDAL.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GC_Capstone5.Models
+{
+    public class MovieDAL
+    {
+        private readonly string _apiKey;
+        private readonly string _apiBearerToken;
+
+        public MovieDAL(IConfiguration configuration)
+        {
+            _apiKey = configuration.GetSection("ApiKeys")["MovieAPI"];
+            _apiBearerToken = configuration.GetSection("ApiBearerTokens")["MovieDbBearerToken"];
+        }
+
+        public HttpClient GetClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.themoviedb.org");
+            client.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiBearerToken);
+            return client;
+        }
+
+        public async Task<string> GetRawJSON(int movieId)
+        {
+            var client = GetClient();
+            var response = await client.GetAsync($"/3/movie/{movieId}?api_key={_apiKey}");
+            var movieJSON = await response.Content.ReadAsStringAsync();
+            return movieJSON;
+        }
+        public async Task<Movie> GetMovie(int movieId)
+        {
+            var client = GetClient();
+            var response = await client.GetAsync($"/3/movie/{movieId}");
+//            var response = await client.GetAsync($"/3/movie/{movieId}?api_key={_apiKey}");
+            var movieJSON = await response.Content.ReadAsStringAsync();
+            Movie movie = JsonSerializer.Deserialize<Movie>(movieJSON);
+            return movie;
+        }
+    }
+}
